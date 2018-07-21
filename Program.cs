c278adfa785bdfbe50efef8c6831213bcca6a661
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-07-21 11:18:59 -0400

    [Add] Arq. MVC [Add] CONFIGURING DEPENDENCY INJECTION

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebShop
+namespace BethanysPieShop
 {
     public class Program
     {
