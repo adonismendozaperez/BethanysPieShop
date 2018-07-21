0fc739c3236b314df96f9380884c718657d2df60
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-07-21 11:34:49 -0400

    [Add] PieController

---
+++ Controllers/PieController.cs
@@ -0,0 +1,27 @@
+namespace BethanysPieShop.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using BethanysPieShop.Models;
+    using WebShop.Repositories;
+
+    public class PieController : Controller
+    {
+        private  IPieRepository _pieRepository;      
+        private  ICategoryRepository _categoryRepository;
+        
+
+        public ViewResult List([FromServices] IPieRepository PieRepository, [FromServices] ICategoryRepository CategoryRepository )
+        {
+            _pieRepository = PieRepository;
+            _categoryRepository = CategoryRepository;
+
+            return View(_pieRepository.Pies);
+        }
+
+    }
+}
